e5356d59807c9a56bf8872c30bf4a0250bf17595
sho <Pot-8-Os@users.noreply.github.com>
2024-03-10 12:36:17 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          やっていきまっしょう！！
+          ただいま開発中です！！
         </a>
       </header>
     </div>
